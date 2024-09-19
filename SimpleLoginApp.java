cd56082edd1ea9ee68c60c1a910657ed47b323cd
AzolaM <st10435525@rcconnect.edu.za>
2024-09-19 10:04:50 +0000

    changed code

---
+++ SimpleLoginApp.java
@@ -0,0 +1,13 @@
+public class SimpleLoginApp{
+
+}
+
+private static void simpleLgin(Scanner scanner) {
+    System.out.printli( "Login App" );
+    
+    // Dog name validation
+    System.out.print(s: " Enter the dog's name: ");
+    String dogName = scanner.nextLine();
+    
+}
+
